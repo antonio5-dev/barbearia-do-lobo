1303e1075c1a4e603db3e5a86d0afb6018ba4f07
Antônio Júnior <antonio5marcio6@gmail.com>
2026-05-17 17:32:42 -0300

    versão Barbearia com Chatbot simples

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ const sendChat = document.getElementById("send-chat");
 const chatInput = document.getElementById("chat-input");
 const chatMessages = document.getElementById("chatbot-messages");
 
+let chatState = "normal";
+
 sendChat.addEventListener("click", () => {
 
     const mensagem = chatInput.value;
@@ -40,6 +42,91 @@ sendChat.addEventListener("click", () => {
     // limpa input
     chatInput.value = "";
 
+    // fluxo de agendamento
+if (chatState === "waiting_schedule") {
+
+    if (
+        mensagem.toLowerCase().includes("sim")
+    ) {
+
+        const respostaBot = document.createElement("div");
+
+        respostaBot.textContent =
+        "Perfeito! Vou transferir você para nosso atendimento no WhatsApp.";
+
+        respostaBot.style.background = "black";
+        respostaBot.style.padding = "10px";
+        respostaBot.style.marginBottom = "10px";
+        respostaBot.style.borderRadius = "10px";
+        respostaBot.style.width = "fit-content";
+
+        chatMessages.appendChild(respostaBot);
+
+        window.open(
+            "https://wa.me/5571982905351",
+            "_blank"
+        );
+
+        chatState = "normal";
+
+        return;
+    }
+
+    if (
+        mensagem.toLowerCase().includes("não") ||
+        mensagem.toLowerCase().includes("nao")
+    ) {
+
+        const respostaBot = document.createElement("div");
+
+        respostaBot.textContent =
+        "Certo 😊 Quer mais alguma informação?";
+
+        respostaBot.style.background = "black";
+        respostaBot.style.padding = "10px";
+        respostaBot.style.marginBottom = "10px";
+        respostaBot.style.borderRadius = "10px";
+        respostaBot.style.width = "fit-content";
+
+        chatMessages.appendChild(respostaBot);
+
+        chatState = "waiting_more_info";
+
+        return;
+    }
+}
+
+if (chatState === "waiting_more_info") {
+
+    if (
+        mensagem.toLowerCase().includes("não") ||
+        mensagem.toLowerCase().includes("nao")
+    ) {
+
+        const respostaBot = document.createElement("div");
+
+        respostaBot.textContent =
+        "Obrigado pelo contato! Até logo 👋";
+
+        respostaBot.style.background = "black";
+        respostaBot.style.padding = "10px";
+        respostaBot.style.marginBottom = "10px";
+        respostaBot.style.borderRadius = "10px";
+        respostaBot.style.width = "fit-content";
+
+        chatMessages.appendChild(respostaBot);
+
+        setTimeout(() => {
+
+            document.getElementById("chatbot")
+            .style.display = "none";
+
+        }, 2000);
+
+        return;
+    }
+}
+
     // resposta automática fake
     setTimeout(() => {
 
@@ -48,39 +135,41 @@ sendChat.addEventListener("click", () => {
         let resposta = "";
 
 if (
-    mensagem.toLowerCase().includes("site") ||
-    mensagem.toLowerCase().includes("sites")
+    mensagem.toLowerCase().includes("preço") ||
+    mensagem.toLowerCase().includes("serviços")
 ) {
 
     resposta =
-    "Criamos sites modernos, rápidos e responsivos para empresas e profissionais que querem atrair mais clientes 😄";
+    "Esses são nossos serviços: Corte: Adulto - R$ 70,00 | Infantil - R$ 45,00; Barba: R$ 50,00; Corte + Barba; R$ 110,00";
 
 }
 
 else if (
-    mensagem.toLowerCase().includes("chatbot") ||
-    mensagem.toLowerCase().includes("ia")
+    mensagem.toLowerCase().includes("localização") ||
+    mensagem.toLowerCase().includes("endereço")
 ) {
 
     resposta =
-    "Também desenvolvemos chatbots integrados ao site para atendimento automático e geração de clientes 🤖";
+    "Estamos na Rua Alcateia, número 518, Boca do Rio. Fica ao lado da Sorveteria Pimenta";
 
 }
 
 else if (
-    mensagem.toLowerCase().includes("suporte") ||
-    mensagem.toLowerCase().includes("manutenção")
+    mensagem.toLowerCase().includes("horário") ||
+    mensagem.toLowerCase().includes("funciona") ||
+    mensagem.toLowerCase().includes("abertos") ||
+    mensagem.toLowerCase().includes("aberta")
 ) {
 
     resposta =
-    "Oferecemos suporte, manutenção e atualizações contínuas para manter seu site sempre funcionando 🚀";
+    "Funcionamos de Terça a Sexta: das 10h às 19h; aos Sábados e Domingos: das 11h às 20h; Será uma satisfação atendê-lo!"
 
 }
 
 else {
 
     resposta =
-    "Olá 😄 Podemos ajudar com sites profissionais, chatbots com IA e suporte técnico.";
+    "Olá 😄 Aqui você recerá o atendimento diferenciado que merece.";
 
 }
 
@@ -93,6 +182,25 @@ respostaBot.textContent = resposta;
         respostaBot.style.width = "fit-content";
 
         chatMessages.appendChild(respostaBot);
+
+        setTimeout(() => {
+
+        const perguntaBot = document.createElement("div");
+
+        perguntaBot.textContent =
+        "Quer agendar horário?";
+
+        perguntaBot.style.background = "black";
+        perguntaBot.style.padding = "10px";
+        perguntaBot.style.marginBottom = "10px";
+        perguntaBot.style.borderRadius = "10px";
+        perguntaBot.style.width = "fit-content";
+
+        chatMessages.appendChild(perguntaBot);
+
+        chatState = "waiting_schedule";
+
+        }, 1000); 
 
     }, 500);
 
